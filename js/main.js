88b73a64c754b7556c76ce25dce9868f60eab31b
hitsan <soledewa2828@gmail.com>
2021-05-30 12:33:16 +0900

    add show text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let element = document.getElementById('msg_box');
-let button = document.getElementById('send_btn');
+let $mess = document.getElementById('send_box');
+let $button = document.getElementById('send_btn');
+const $caht_area = document.getElementById('chat');
 
-button.addEventListener('click', () => {
-    document.write(element.value);
+const clickHandler = () => {
+    let li = document.createElement('li');
+    let text = document.createTextNode($mess.value);
+    console.log($mess.value);
+    console.log(text);
+
+    li.appendChild(text);
+    console.log(li);
+    console.log($button);
+    $caht_area.appendChild(li);
+}
+$button.addEventListener('click', () => {
+    clickHandler();
 });
